eeb311d86c6364420ee0c1af59716df407885af1
Remy Prechelt <rprechelt@gmail.com>
2017-08-05 03:05:48 -0500

    Added setup.py script for pip install

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='atlas-client',
+      version='0.1',
+      description='Client package for Atlas telescopes',
+      long_description=open('README.md').read(),
+      license='LICENSE', 
+      author='Remy Prechelt',
+      author_email='rprechelt@uchicago.edu',
+      url='https://github.com/yerkesobservatory/atlas-client',
+      packages=['telescope'],
+      install_requires=[
+          'colorlog>=3.0.1',
+          'gevent>=1.2.2',
+          'greenlet>=0.4.12',
+          'websocket>=0.2.1']
+)
